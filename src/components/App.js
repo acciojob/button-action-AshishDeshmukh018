b5465e14189105ee609f63f8cc52c74b9169ac28
AshishDeshmukh018 <126492153+AshishDeshmukh018@users.noreply.github.com>
2023-03-29 12:17:35 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,19 @@ import './../styles/App.css';
 
 const App = (props) => {
 
+  const button = document.getElementById("click");
+  const para = document.getElementById("para");
+
+  button.addEventListener("click", function() {
+  para.classList.remove("hide");
+  para.classList.add("show");
+});
+
   return (
     <div className="App" id="main">
       // Do not alter the main div
+      <button id="click">Click me</button>
+      <p id="para" class="hide">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
     </div>
   );
 }
